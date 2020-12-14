eaf5d391d2ff9661db6fa78f12b2e0c835179e4f
LipmannMike <Lypka.michael@gmail.com>
2020-12-14 23:32:18 +0300

    added homework lesson 5 task 2 (Must be upgrade later)

---
+++ HW_lesson5-2.py
@@ -0,0 +1,39 @@
+"""
+Дана дата в формате dd.mm.yyyy, например: 02.11.2013.
+Ваша задача — вывести дату в текстовом виде, например: второе ноября 2013 года.
+Склонением пренебречь (2000 года, 2010 года)
+"""
+
+date = '09.05.2013'
+
+d, m, y = date.split('.')
+
+day = {
+    '01': 'First',
+    '02': 'Second',
+    '03': 'Third',
+    '04': 'Forth',
+    '05': 'Fifth',
+    '06': 'Sixth',
+    '07': 'Seventh',
+    '08': 'Eighth',
+    '09': 'Ninth',
+    '10': 'Tenth',
+    '11': 'Eleventh'
+}
+
+month = {
+    '01': 'January',
+    '02': 'February',
+    '03': 'March',
+    '04': 'April',
+    '05': 'May',
+    '06': 'June',
+    '07': 'July',
+    '08': 'August',
+    '09': 'September',
+    '10': 'October',
+    '11': 'November',
+    '12': 'December'
+}
+print(f'{day[d]} {month[m]} 2013 year')
